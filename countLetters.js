7dfc8a33828e00a99b73b414a50ca6d7c779c629
Dahlia75 <d.aljabari75@gmail.com>
2018-04-11 21:14:39 +0000

    write the function

--- countLetters.js
+++ countLetters.js
@@ -1,3 +1,14 @@
-function countLetters(){
+function countLetters(sentance){
+  var count={};
 
+  for(var i = 0; i < sentance.length; i++){
+    if(sentance[i] !== " "){
+      if(!count[sentance[i]]) {
+        count[sentance[i]] = 0;
+      }
+      count[sentance[i]] += 1;
+    }
+  }
+  console.log(count);
 }
+countLetters("lighthouse in the house")
